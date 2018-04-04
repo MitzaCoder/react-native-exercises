116be37dc3997ce86e149d5f55de18b8ca2d885f
Mihai Saru <mihai.saru@qualitance.com>
2018-04-04 16:07:43 +0300

    Code refactoring, add exercise requirement

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   View,
   Text,
   TextInput,
+  StyleSheet,
 } from 'react-native';
-
 class App extends Component {
   state = {
     firstName: '',
     lastName: '',
-    ageField: '',
+    age: '',
     focusedField: '',
     lastBluredField: '',
     lastSubmitedValue: '',
@@ -18,67 +17,70 @@ class App extends Component {
   };
 
   render() {
+    const {
+      firstName,
+      lastName,
+      age,
+      focusedField,
+      lastBluredField,
+      lastSubmitedValue,
+      lastEditedValue,
+    } = this.state;
     return (
       <View style={styles.container}>
-        <View style={{
-          width: 50,
-          height: 50,
-          borderRadius: 25,
-          backgroundColor: this.state.blured ? '#00F' : '#F00',
-        }}>{}</View>
-        <Text>Prenume: {this.state.firstName}</Text>
-        <Text>Nume: {this.state.lastName}</Text>
-        <Text>Varsta: {this.state.ageField}</Text>
-        <Text>Focused field: {this.state.focusedField}</Text>
-        <Text>Last blured field: {this.state.lastBluredField}</Text>
-        <Text>Last submited value: {this.state.lastSubmitedValue}</Text>
-        <Text>Last edited value: {this.state.lastEditedValue}</Text>
+        <Text>Prenume: {firstName}</Text>
+        <Text>Nume: {lastName}</Text>
+        <Text>Age: {age}</Text>
+        <Text>Focused field: {focusedField}</Text>
+        <Text>Last blured field: {lastBluredField}</Text>
+        <Text>Last submited value: {lastSubmitedValue}</Text>
+        <Text>Last edited value: {lastEditedValue}</Text>
         <TextInput
           style={styles.textInput}
-          onChangeText={firstName => this.setState({firstName})}
-          onFocus={() => this.setState({focusedField: 'Prenume'})}
-          onBlur={() => this.setState({lastBluredField: 'Prenume'})}
-          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
-          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
           value={this.state.firstName}
+          onChangeText={firstName => this.setState({firstName})}
+          onBlur={() => this.setState({lastBluredField: 'Prenume'})}
+          onFocus={() => this.setState({focusedField: 'Prenume'})}
+          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
+          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
           placeholder="Prenume"
         />
         <TextInput
           style={styles.textInput}
-          onChangeText={lastName => this.setState({lastName})}
-          onFocus={() => this.setState({focusedField: 'Nume'})}
-          onBlur={() => this.setState({lastBluredField: 'Nume'})}
-          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
-          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
           value={this.state.lastName}
+          onChangeText={lastName => this.setState({lastName})}
+          onBlur={() => this.setState({lastBluredField: 'Nume'})}
+          onFocus={() => this.setState({focusedField: 'Nume'})}
+          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
+          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
           placeholder="Nume"
         />
         <TextInput
           style={styles.textInput}
-          onChangeText={age => this.setState({ageField: age})}
-          onFocus={() => this.setState({focusedField: 'Varsta'})}
+          value={this.state.age}
+          onChangeText={age => this.setState({age})}
           onBlur={() => this.setState({lastBluredField: 'Varsta'})}
-          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
+          onFocus={() => this.setState({focusedField: 'Varsta'})}
           onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
-          value={this.state.ageField}
+          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
           placeholder="Varsta"
         />
       </View>
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FFF',
   },
   textInput: {
     width: '50%',
     height: 50,
-  },
+  }
 });
 
 export default App;
