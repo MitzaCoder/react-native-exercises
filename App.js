da6d47ee462403b9d1f407fdef6e0fd4fdb52103
Mihai Saru <mihai.saru@qualitance.com>
2018-04-04 16:09:07 +0300

    Remove exercise code

--- App.js
+++ App.js
@@ -35,36 +35,6 @@ class App extends Component {
         <Text>Last blured field: {lastBluredField}</Text>
         <Text>Last submited value: {lastSubmitedValue}</Text>
         <Text>Last edited value: {lastEditedValue}</Text>
-        <TextInput
-          style={styles.textInput}
-          value={this.state.firstName}
-          onChangeText={firstName => this.setState({firstName})}
-          onBlur={() => this.setState({lastBluredField: 'Prenume'})}
-          onFocus={() => this.setState({focusedField: 'Prenume'})}
-          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
-          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
-          placeholder="Prenume"
-        />
-        <TextInput
-          style={styles.textInput}
-          value={this.state.lastName}
-          onChangeText={lastName => this.setState({lastName})}
-          onBlur={() => this.setState({lastBluredField: 'Nume'})}
-          onFocus={() => this.setState({focusedField: 'Nume'})}
-          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
-          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
-          placeholder="Nume"
-        />
-        <TextInput
-          style={styles.textInput}
-          value={this.state.age}
-          onChangeText={age => this.setState({age})}
-          onBlur={() => this.setState({lastBluredField: 'Varsta'})}
-          onFocus={() => this.setState({focusedField: 'Varsta'})}
-          onSubmitEditing={event => this.setState({lastSubmitedValue: event.nativeEvent.text})}
-          onEndEditing={event => this.setState({lastEditedValue: event.nativeEvent.text})}
-          placeholder="Varsta"
-        />
       </View>
     );
   }
